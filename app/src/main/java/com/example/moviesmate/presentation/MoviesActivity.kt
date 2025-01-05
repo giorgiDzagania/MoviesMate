8c42f7f494fe76244bc0032ec774e0d5d2378c9b
giorgiDzagania <georgedzazgania@gmail.com>
2025-01-05 14:53:36 +0400

    first commit

---
+++ app/src/main/java/com/example/moviesmate/presentation/MoviesActivity.kt
@@ -0,0 +1,17 @@
+package com.example.moviesmate.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.moviesmate.R
+
+class MoviesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_movies)
+
+    }
+}
