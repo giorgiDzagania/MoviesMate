b0e6a93775e18795c8f4c917d779887c3c87d3a2
GFO <gia.focxverashvili@gmail.com>
2025-01-05 22:29:14 +0400

    refactor

--- app/src/main/java/com/example/moviesmate/presentation/screens/BaseFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.moviesmate.presentation.screens
-
-import android.view.LayoutInflater
-import android.view.ViewGroup
-
-typealias Inflater<VB> = (LayoutInflater, ViewGroup?, Boolean) -> VB
-
