a999a39e7974913d96680673c8a729141efc25a3
giorgiDzagania <georgedzazgania@gmail.com>
2025-01-05 19:52:26 +0400

    add binding to Movies Activity

--- app/src/main/java/com/example/moviesmate/presentation/MoviesActivity.kt
+++ app/src/main/java/com/example/moviesmate/presentation/MoviesActivity.kt
@@ -10,11 +10,9 @@ import com.example.moviesmate.databinding.ActivityMoviesBinding
 
 class MoviesActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMoviesBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMoviesBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
